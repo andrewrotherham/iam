efe9bb9dd084efc46f76dda9633b388192f8fcb2
andrewrotherham <66618878+andrewrotherham@users.noreply.github.com>
2020-11-29 13:20:52 +0000

    Add files via upload

---
+++ jenkins/variable.tf
@@ -0,0 +1,4 @@
+variable ami { default = "ami-09b9e380df60300c8" }
+variable region { default = "eu-west-1" }
+variable project { default = "jenkins" }
+variable mysystem { default = "86.4.102.110/32" }
